49476e15f2f00c84daf743a8c74d465526aa0064
zaqqqq808 <zaq@lengmedia.com>
2026-06-21 15:07:17 +0800

    Rename 'Cheat Sheet Tutorials' to 'AI Cheat Sheet Tutorials'

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -36,7 +36,7 @@ export default function Nav() {
           <li><Link href="/#services">Services</Link></li>
           <li><Link href="/blog">Blog</Link></li>
           <li><Link href="/free-tools">Resources</Link></li>
-          <li><Link href="/ai-software-tutorials" className={styles.course}>Cheat Sheet Tutorials</Link></li>
+          <li><Link href="/ai-software-tutorials" className={styles.course}>AI Cheat Sheet Tutorials</Link></li>
           <li><Link href="/ecommerce-protocol" className={styles.course}>Ecom Launch Protocol</Link></li>
           <li><Link href="/business-enquiry" className={styles.enquiry}>Business Enquiry</Link></li>
           <li className={styles.authControls}>
@@ -68,7 +68,7 @@ export default function Nav() {
           <li><Link href="/#services" onClick={close}>Services</Link></li>
           <li><Link href="/blog" onClick={close}>Blog</Link></li>
           <li><Link href="/free-tools" onClick={close}>Resources</Link></li>
-          <li><Link href="/ai-software-tutorials" className={styles.mobileCourse} onClick={close}>Cheat Sheet Tutorials</Link></li>
+          <li><Link href="/ai-software-tutorials" className={styles.mobileCourse} onClick={close}>AI Cheat Sheet Tutorials</Link></li>
           <li><Link href="/ecommerce-protocol" className={styles.mobileCourse} onClick={close}>Ecom Launch Protocol</Link></li>
           <li><Link href="/business-enquiry" className={styles.mobileEnquiry} onClick={close}>Business Enquiry</Link></li>
         </ul>
